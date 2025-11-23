afe47d69fad2f41cf0f58907eca21afea9769c0b
Rodolfo Ribeiro Gomes <rodolforg@gmail.com>
2025-11-23 17:25:38 -0300

    refactor: remove unused variables in CanvasView

--- synfig-studio/src/gui/canvasview.h
+++ synfig-studio/src/gui/canvasview.h
@@ -280,8 +280,6 @@ private:
 
 	Gtk::Button *closebutton;
 	Gtk::Button *stopbutton;
-	Gtk::ToggleToolButton *background_rendering_button;
-	Gtk::ToolButton *refreshbutton;
 	Gtk::ComboBoxText *render_combobox;
 	Gtk::Grid *timebar;
 	Gtk::Toolbar *top_toolbar;
@@ -290,7 +288,6 @@ private:
 	Gtk::ToggleButton *animatebutton;
 	Gtk::ToggleButton *timetrackbutton;
 	Gtk::Grid *timetrack;
-	Gtk::Button *keyframebutton;
 	KeyFrameDial *keyframedial;
 	bool toggling_animate_mode_;
 	FrameDial *framedial;
@@ -303,14 +300,6 @@ private:
 	Glib::RefPtr<Gtk::Adjustment> past_onion_adjustment_;
 	Gtk::SpinButton *past_onion_spin;
 	Gtk::SpinButton *future_onion_spin;
-	Gtk::ToggleToolButton *show_grid;
-	Gtk::ToggleToolButton *snap_grid;
-	Gtk::ToggleToolButton *show_guides;
-	Gtk::ToggleToolButton *snap_guides;
-	Gtk::ToggleToolButton *onion_skin;
-	Gtk::ToolButton *render_options_button;
-	Gtk::ToolButton *preview_options_button;
-	Gtk::ToggleToolButton *onion_skin_keyframes;
 
 	//! Shows current time and allows edition
 	Widget_Time *current_time_widget;
@@ -342,6 +331,7 @@ private:
 	Glib::RefPtr<Gio::SimpleAction> low_resolution_toggle;
 	Gtk::RadioButtonGroup low_res_pixel_size_group;
 
+	// Glib::RefPtr<Gtk::ActionGroup> action_group;
 	Glib::RefPtr<Gio::SimpleActionGroup> action_group_;
 	bool _action_group_removed;
 
